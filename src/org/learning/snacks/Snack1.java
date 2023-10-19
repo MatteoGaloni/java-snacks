726c4b9cb8fe2fdafe073f23cce1ea11d78d77b1
MatteoGaloni <mtgaloni@gmail.com>
2023-10-19 14:22:18 +0200

    add Snack1

--- src/org/learning/snacks/Snack1.java
+++ src/org/learning/snacks/Snack1.java
@@ -1,2 +1,23 @@
-package org.learning.snacks;public class Snack1 {
+package org.learning.snacks;
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean isPositive = true;
+
+        while (isPositive){
+            System.out.println("Insert number");
+            int number = Integer.parseInt(scan.nextLine());
+
+            if (number >= 0) {
+                System.out.println(number % 2 == 0 ? number : number + 1);
+            } else if (number < 0){
+                isPositive = false;
+            }
+        }
+
+        scan.close();
+    }
 }
