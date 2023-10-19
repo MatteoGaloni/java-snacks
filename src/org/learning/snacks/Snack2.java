acada2ab2bd0f28b21f00c508c34e45ff53b5cb1
MatteoGaloni <mtgaloni@gmail.com>
2023-10-19 14:40:00 +0200

    add Snack2

---
+++ src/org/learning/snacks/Snack2.java
@@ -0,0 +1,17 @@
+package org.learning.snacks;
+
+import java.util.Random;
+
+public class Snack2 {
+    public static void main(String[] args) {
+
+        String [] names = {"Mario", "Franco", "Gianni", "Francesco", "Claudia", "Maria", "Giovanna", "Sara"};
+        String [] surnames = {"Rossi", "Verdone", "Giallini", "Serra", "Monti", "Zazzaroni", "Mentana", "De Angelis"};
+
+        Random randomGenerator = new Random();
+        for (int i = 0; i < names.length ; i++) {
+        int randomNumber = randomGenerator.nextInt(0, names.length);
+        System.out.println(names[randomNumber] + " " + surnames[randomNumber]);
+        }
+    }
+}
